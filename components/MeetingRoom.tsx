ae2c228c997f743b318732e1510fd9a7678ea1d0
Lichu23 <lisandroxarenax@gmail.com>
2024-09-19 17:15:27 -0300

    feat:Stream Create Meeting

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,17 +1,41 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 import {
   CallControls,
+  CallingState,
   CallParticipantsList,
+  CallStatsButton,
   PaginatedGridLayout,
   SpeakerLayout,
+  useCallStateHooks,
 } from "@stream-io/video-react-sdk";
-import React, { useState } from "react";
 
-type CallLayourTyoe = "grid" | "speaker-left" | "speaker-right";
+import { LayoutList, Users } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import EndCallButton from "./EndCallButton";
+import Loader from "./Loader";
+
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
   const [layout, setLayout] = useState("speaker-left");
   const [showParticipants, setShowParticipants] = useState(false);
+  const searchParams  = useSearchParams()
+  const isPersonalRoom = !!searchParams.get("personal")
+  
+  const {useCallCallingState} = useCallStateHooks()
+  const callingState = useCallCallingState()
+
+  if(callingState !== CallingState.JOINED) return <Loader/>  //loading when us tried to joined call 
+
+
 
   const CallLayout = () => {
     switch (layout) {
@@ -29,7 +53,7 @@ const MeetingRoom = () => {
   return (
     <section className="relative h-screen w-full overflow-hidden text-white pt-4">
       <div className="relative flex size-full items-center justify-center">
-        <div className="flex size-full max-w-[1000px] items-center">
+        <div className="flex size-full max-w-[950px] items-center">
           <CallLayout />
         </div>
 
@@ -42,8 +66,34 @@ const MeetingRoom = () => {
         </div>
       </div>
 
-      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
-          <CallControls/>
+      <div className="fixed flex-wrap max-md:p-1 py-1 bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
+        <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className=" border-dark-1 bg-dark-1 text-white">
+            {["grid", "speaker-left","speaker-right"].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem className="cursor-pointer" onClick={() => {
+                  setLayout(item.toLowerCase() as CallLayoutType)
+                }}>
+                {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-dark-1" />
+              </div>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+            <button onClick={() => setShowParticipants((prev) => !prev)}>
+              <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+                <Users size={20} className="text-white"/>
+              </div>
+            </button>
+            {!isPersonalRoom && <EndCallButton/>}
+        <CallStatsButton/>
       </div>
     </section>
   );
