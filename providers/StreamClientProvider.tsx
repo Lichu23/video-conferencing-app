72dd26cd8140349b1a70039eeca40ae67e753e28
Lichu23 <lisandroxarenax@gmail.com>
2024-09-30 17:01:02 -0300

    Clear comments

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -6,13 +6,7 @@ import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
 import { ReactNode, useEffect, useState } from "react";
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
-// const userId = "user-id";
-// const token = "authentication-token";
-// const user: User = { id: userId };
 
-// const client = new StreamVideoClient({ apiKey, user, token });
-// const call = client.call("default", "my-first-call");
-// call.join({ create: true });
 
 const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
   const [videoClient, setVideoClient] = useState<StreamVideoClient>();
